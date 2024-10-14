6c22d01a688f17c5595caa67688328f180d4af04
Esteban Martinez <arepo90@proton.me>
2024-10-14 15:35:15 -0600

    trak

--- build/ROTAS_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/nabet/Desktop/GUI/ROTAS-ROS2/src/ROTAS_pkg
-# Build directory: C:/Users/nabet/Desktop/GUI/ROTAS-ROS2/build/ROTAS_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
